d684a8309454244a672f48885a107630ab59ab19
Jonh Alex <122692601+Jonhvmp@users.noreply.github.com>
2024-11-18 19:09:41 -0300

    melhora o middleware de autenticação para retornar respostas JSON em vez de passar erros para o middleware de erro

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -1,21 +1,20 @@
-// src/middlewares/authMiddleware.ts
-
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import env from '../config/env';
 
 export const validateToken = (req: Request, res: Response, next: NextFunction): void => {
   const token = req.header('Authorization')?.replace('Bearer ', '');
-
   if (!token) {
-    return next({ status: 401, message: 'Token de autenticação ausente.' }); // Passa o erro para o middleware de erro
+    res.status(401).json({ message: 'Token de autenticação ausente.' });
+    return;
   }
 
   try {
-    const decoded = jwt.verify(token, env.JWT_SECRET as string) as { id: string };
-    req.user = { id: decoded.id }; // Adiciona a propriedade `user` no objeto da requisição
+    const decoded = jwt.verify(token, env.JWT_SECRET) as { id: string };
+    req.user = { id: decoded.id };
     next();
+    return;
   } catch (error) {
-    next({ status: 401, message: 'Token de autenticação inválido.' }); // Passa o erro para o middleware de erro
+    res.status(401).json({ message: 'Token de autenticação inválido.' });
   }
 };
