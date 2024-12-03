709058bfa505d0194cbb000c85929be302fe696b
Jonh Alex <122692601+Jonhvmp@users.noreply.github.com>
2024-12-03 14:35:07 -0300

    refatora controlador de autenticação para usar variáveis de ambiente e melhora a validação de entradas, incluindo tratamento de erros e registro de eventos

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -2,18 +2,17 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import { User } from '../models/User';
-import dotenv from 'dotenv';
 import crypto from 'crypto';
 import { Token } from '../models/Token';
-
-dotenv.config();
+import env from '../config/env';
+import { sendEmail } from '../config/sendEmail';
 
 const generateAccessToken = (user: any) => {
-  return jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET as string, { expiresIn: '1h' });
+  return jwt.sign({ id: user.id, email: user.email }, env.JWT_SECRET as string, { expiresIn: '1h' });
 };
 
 const generateRefreshToken = (user: any) => {
-  return jwt.sign({ id: user.id }, process.env.JWT_SECRET as string, { expiresIn: '7d' });
+  return jwt.sign({ id: user.id }, env.JWT_SECRET as string, { expiresIn: '7d' });
 };
 
 const validateEmail = (email: string): boolean => {
@@ -26,6 +25,7 @@ const validatePassword = (password: string, confirmPassword: string): boolean =>
 };
 
 const handleValidationError = (res: Response, message: string): void => {
+  console.log(`Erro de validação: ${message}`);
   res.status(400).json({ message });
 };
 
@@ -34,46 +34,43 @@ export const registerUser = async (req: Request, res: Response, next: NextFuncti
     const { username, email, password, confirmPassword } = req.body;
 
     if (!username || !email || !password || !confirmPassword) {
-      handleValidationError(res, 'Todos os campos são obrigatórios');
-      return;
+      return handleValidationError(res, 'Todos os campos são obrigatórios');
     }
 
     if (username.length < 3 || username.length > 50) {
-      handleValidationError(res, 'O nome de usuário é obrigatório e deve ter entre 3 e 50 caracteres.');
-      return;
+      return handleValidationError(res, 'O nome de usuário é obrigatório e deve ter entre 3 e 50 caracteres.');
     }
 
     if (!validateEmail(email)) {
-      handleValidationError(res, 'O e-mail é obrigatório e deve ser válido.');
-      return;
+      return handleValidationError(res, 'O e-mail é obrigatório e deve ser válido.');
     }
 
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      handleValidationError(res, 'Email já está em uso');
-      return;
+      return handleValidationError(res, 'Email já está em uso');
     }
 
     const existingUsername = await User.findOne({ username });
     if (existingUsername) {
-      handleValidationError(res, 'Nome de usuário já está em uso');
-      return;
+      return handleValidationError(res, 'Nome de usuário já está em uso');
     }
 
     if (!validatePassword(password, confirmPassword)) {
-      handleValidationError(res, 'A senha é obrigatória e deve ter entre 8 e 128 caracteres e as senhas devem coincidir.');
-      return;
+      return handleValidationError(res, 'A senha é obrigatória e deve ter entre 8 e 128 caracteres e as senhas devem coincidir.');
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({ username, email, password: hashedPassword });
     await user.save();
 
+    console.log(`Usuário registrado: ${user.id}`);
+
     const accessToken = generateAccessToken(user);
     const refreshToken = generateRefreshToken(user);
 
     res.status(201).json({ message: 'Usuário registrado com sucesso', accessToken, refreshToken });
   } catch (error) {
+    console.error(`Erro ao registrar usuário: ${error as Error}`);
     next(error);
   }
 };
@@ -83,44 +80,54 @@ export const loginUser = async (req: Request, res: Response, next: NextFunction)
     const { email, password } = req.body;
 
     if (!email || !password) {
-      handleValidationError(res, 'Todos os campos são obrigatórios');
-      return;
+      return handleValidationError(res, 'Todos os campos são obrigatórios');
     }
 
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email }).select('+password'); // Inclui a senha na consulta
     if (!user) {
-      handleValidationError(res, 'Credenciais inválidas');
-      return;
+      return handleValidationError(res, 'Credenciais inválidas');
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      handleValidationError(res, 'Credenciais inválidas');
-      return;
+    // Verifica se o usuário está bloqueado
+    if (user.isLocked()) {
+      return handleValidationError(res, 'Conta bloqueada devido a várias tentativas de login. Tente novamente mais tarde.');
     }
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      await user.incrementLoginAttempts(); // Incrementa tentativas de login
+      return handleValidationError(res, 'Credenciais inválidas');
+    }
+
+    // Login bem-sucedido: redefinir tentativas de login
+    user.loginAttempts = 0;
+    user.lockUntil = null; // Remove bloqueios, caso tenha
+    await user.save();
+
+    console.log(`Usuário logado: ${user.id}`);
 
     const accessToken = generateAccessToken(user);
     const refreshToken = generateRefreshToken(user);
 
     res.json({ accessToken, refreshToken });
   } catch (error) {
+    console.error(`Erro ao fazer login: ${error as Error}`);
     next(error);
   }
 };
+
 
 export const forgotPassword = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     const { email } = req.body;
 
     if (!email) {
-      handleValidationError(res, 'O e-mail é obrigatório');
-      return;
+      return handleValidationError(res, 'O e-mail é obrigatório');
     }
 
     const user = await User.findOne({ email });
     if (!user) {
-      handleValidationError(res, 'Usuário não encontrado');
-      return;
+      return handleValidationError(res, 'Usuário não encontrado');
     }
 
     const resetToken = crypto.randomBytes(32).toString('hex');
@@ -129,15 +136,29 @@ export const forgotPassword = async (req: Request, res: Response, next: NextFunc
     const token = new Token({
       userId: user._id,
       token: hashedToken,
-      expiresAt: Date.now() + 20 * 60 * 1000 // 20 minutes
+      expiresAt: Date.now() + 20 * 60 * 1000 // 20 minutos
     });
 
     await token.save();
 
+    console.log(`Token de redefinição de senha gerado para o usuário: ${user.id}`);
+
     const resetLink = `${req.protocol}://${req.get('host')}/reset-password/${resetToken}`;
+
+    const html = `
+    <h1>Redefinição de Senha</h1>
+      <p>Olá, ${user.username}!</p>
+      <p>Você solicitou a redefinição de sua senha. Clique no link abaixo para continuar:</p>
+      <a href="${resetLink}">${resetLink}</a>
+      <p>O link expira em 20 minutos.</p>
+      <p>Se você não solicitou esta ação, ignore este e-mail.</p>
+    `;
+
+    await sendEmail(user.email, 'Redefinição de Senha', html);
 
     res.json({ message: 'E-mail de redefinição de senha enviado com sucesso', resetLink });
   } catch (error) {
+    console.error(`Erro ao solicitar redefinição de senha: ${(error as Error).message}`);
     next(error);
   }
 };
@@ -146,40 +167,47 @@ export const resetPassword = async (req: Request, res: Response, next: NextFunct
   try {
     const { token, password, confirmPassword } = req.body;
 
+    // Validação básica
     if (!token || !password || !confirmPassword) {
-      handleValidationError(res, 'Todos os campos são obrigatórios');
-      return;
+      return handleValidationError(res, 'Todos os campos são obrigatórios');
     }
 
     if (!validatePassword(password, confirmPassword)) {
-      handleValidationError(res, 'A senha é obrigatória e deve ter entre 8 e 128 caracteres e as senhas devem coincidir.');
-      return;
+      return handleValidationError(res, 'A senha é obrigatória e deve ter entre 8 e 128 caracteres e as senhas devem coincidir.');
     }
 
+    // Validação do token de redefinição de senha
     const hashedToken = crypto.createHash('sha256').update(token).digest('hex');
     const tokenDoc = await Token.findOne({ token: hashedToken, expiresAt: { $gt: Date.now() } });
 
     if (!tokenDoc) {
-      handleValidationError(res, 'Token inválido ou expirado');
-      return;
+      return handleValidationError(res, 'Token inválido ou expirado');
     }
 
+    // Busca o usuário associado ao token
     const user = await User.findById(tokenDoc.userId);
     if (!user) {
-      handleValidationError(res, 'Usuário não encontrado');
-      return;
+      return handleValidationError(res, 'Usuário não encontrado');
     }
 
+    // Atualiza a senha do usuário
     user.password = await bcrypt.hash(password, 10);
+    user.loginAttempts = 0; // Reseta tentativas de login
+    user.lockUntil = null; // Remove bloqueios, se houver
     await user.save();
 
+    // Remove o token de redefinição após uso
     await Token.deleteOne({ _id: tokenDoc._id });
 
+    console.log(`Senha redefinida para o usuário: ${user.id}`);
+
+    // Gera novos tokens de acesso
     const accessToken = generateAccessToken(user);
     const refreshToken = generateRefreshToken(user);
 
     res.json({ message: 'Senha redefinida com sucesso', accessToken, refreshToken });
   } catch (error) {
+    console.error(`Erro ao redefinir senha: ${error as Error}`);
     next(error);
   }
 };
