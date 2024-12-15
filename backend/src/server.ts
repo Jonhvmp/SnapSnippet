78cc2a777c235919e1d97a38010ca0cdacbaa572
Jonh Alex <122692601+Jonhvmp@users.noreply.github.com>
2024-12-15 01:13:32 -0300

    Adiciona tratamento de erro para porta em uso e implementa funções de geração de tokens de acesso e atualização; adiciona validações de email e senha

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,13 +2,22 @@ import app from './app';
 import env from './config/env';
 import './types/';
 
-const PORT = env.PORT || '';
+const PORT = env.PORT || 3000;
 
 // teste de rota inicial ('/)
 app.get('get', (req, res) => {
   res.json({ message: 'API rodando...' });
 })
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Servidor rodando na porta http://localhost:${PORT}`);
+});
+
+server.on('error', (err: any) => {
+  if (err.code === 'EADDRINUSE') {
+    console.error(`Port ${PORT} é inválida`);
+    process.exit(1);
+  } else {
+    throw err;
+  }
 });
